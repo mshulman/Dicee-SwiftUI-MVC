809b3297b612fded9ec161c4f06df0d1b15c02cd
Michael Shulman <michael.shulman@hotmail.com>
2019-10-23 10:25:42 -0700

    Initial Commit

---
+++ Dicee-SwiftUI-MVC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI-MVC
+//
+//  Created by Michael Shulman on 10/23/19.
+//  Copyright © 2019 Michael Shulman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
